4a45b94b4904b1b246bded17c069eeb5b8ce570f
Jaren Garman <jarengarman@protonmail.com>
2025-04-28 13:33:57 -0400

    test: Add tests for Customer relationships and validations

--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -1,5 +1,14 @@
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.describe Customer, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "Relationships" do
+    it { is_expected.to have_many :customer_subscriptions }
+  end
+
+  describe "Validations" do
+    it { is_expected.to validate_presence_of :first_name }
+    it { is_expected.to validate_presence_of :last_name }
+    it { is_expected.to validate_presence_of :email }
+    it { is_expected.to validate_presence_of :address }
+  end
 end
