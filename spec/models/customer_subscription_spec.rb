fc35d997bf02222ab36f5146c3a8c46750e1f727
Jaren Garman <jarengarman@protonmail.com>
2025-04-28 13:42:51 -0400

    test: Add tests for CustomerSubscription relationships and validations

--- spec/models/customer_subscription_spec.rb
+++ spec/models/customer_subscription_spec.rb
@@ -1,5 +1,14 @@
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.describe CustomerSubscription, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "Relationships" do
+    it { is_expected.to belong_to :customer }
+    it { is_expected.to belong_to :subscription }
+  end
+
+  describe "Validations" do
+    it { is_expected.to validate_presence_of :status }
+    it { is_expected.to validate_presence_of :frequency }
+    it { is_expected.to validate_numericality_of(:frequency).is_greater_than 0 }
+  end
 end
