867c62c4cd8e62ec95578a4496e7cb946fb33c63
Jaren Garman <jarengarman@protonmail.com>
2025-04-28 16:17:01 -0400

    feat: Override prefix for customer_subscriptions

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :subscriptions, only: :index
-      patch "customer_subscriptions/:id", to: "customer_subscriptions#update"
+      patch "customer_subscriptions/:id", to: "customer_subscriptions#update", as: "customer_subscriptions"
     end
   end
 end
