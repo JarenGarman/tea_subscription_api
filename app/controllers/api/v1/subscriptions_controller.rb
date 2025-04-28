c8c83b1a8f97665350a2ecfdb7f4de15a12e3dd2
Jaren Garman <jarengarman@protonmail.com>
2025-04-28 15:56:22 -0400

    feat: Add sorting and error handling to index

--- app/controllers/api/v1/subscriptions_controller.rb
+++ app/controllers/api/v1/subscriptions_controller.rb
@@ -1,5 +1,21 @@
 class Api::V1::SubscriptionsController < ApplicationController
   def index
-    render json: SubscriptionSerializer.new(Subscription.all), status: :ok
+    if sort_params[:sort_by_price] == "desc"
+      subs = Subscription.order(price: :desc)
+    elsif sort_params[:sort_by_price] == "asc"
+      subs = Subscription.order(price: :asc)
+    elsif sort_params[:sort_by_price].present?
+      render json: ErrorSerializer.format_error(ErrorMessage.new("Value of sort_by_price must be 'asc' or 'desc'", 422)), status: :unprocessable_entity
+      return
+    else
+      subs = Subscription.all
+    end
+    render json: SubscriptionSerializer.new(subs), status: :ok
+  end
+
+  private
+
+  def sort_params
+    params.permit(:sort_by_price)
   end
 end
