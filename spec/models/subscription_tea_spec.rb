82f1668183566bff5f74d6dede2ece207b810e18
Jaren Garman <jarengarman@protonmail.com>
2025-04-28 13:41:31 -0400

    test: Add relationship tests for SubscriptionTea

--- spec/models/subscription_tea_spec.rb
+++ spec/models/subscription_tea_spec.rb
@@ -1,5 +1,8 @@
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.describe SubscriptionTea, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "Relationships" do
+    it { is_expected.to belong_to :tea }
+    it { is_expected.to belong_to :subscription }
+  end
 end
