50eee2b46a7e0aa3d27edd06a1b4ebb49a4b3ea6
Jaren Garman <jarengarman@protonmail.com>
2025-04-28 17:14:09 -0400

    feat: Add customer_subscriptions to SubscriptionSerializer

--- app/serializers/subscription_serializer.rb
+++ app/serializers/subscription_serializer.rb
@@ -2,5 +2,9 @@ class SubscriptionSerializer
   include JSONAPI::Serializer
   attributes :title, :price, :image_url
 
+  attribute :customer_subscriptions do |sub|
+    CustomerSubscriptionSerializer.new(sub.customer_subscriptions)
+  end
+
   has_many :teas
 end
