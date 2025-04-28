797100223ec2b4edb485a8217ccd31a10ef48941
Jaren Garman <jarengarman@protonmail.com>
2025-04-28 13:32:25 -0400

    test: Add tests for Tea relationships and validations

--- spec/models/tea_spec.rb
+++ spec/models/tea_spec.rb
@@ -1,5 +1,15 @@
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.describe Tea, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "Relationships" do
+    it { is_expected.to have_many :subscription_teas }
+  end
+
+  describe "Validations" do
+    it { is_expected.to validate_presence_of :title }
+    it { is_expected.to validate_presence_of :description }
+    it { is_expected.to validate_presence_of :brew_time }
+    it { is_expected.to validate_numericality_of(:brew_time).is_greater_than 0 }
+    it { is_expected.to validate_presence_of :image_url }
+  end
 end
