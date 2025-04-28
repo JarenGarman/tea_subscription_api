1ce0ecb290feceef66f1c88a479299cfa023df77
Jaren Garman <jarengarman@protonmail.com>
2025-04-28 13:32:34 -0400

    feat: Add Tea relationships and validations

--- app/models/tea.rb
+++ app/models/tea.rb
@@ -1,2 +1,6 @@
 class Tea < ApplicationRecord
+  has_many :subscription_teas
+
+  validates :title, :description, :brew_time, :image_url, presence: true
+  validates :brew_time, numericality: {greater_than: 0}
 end
