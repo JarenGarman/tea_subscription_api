3306154fe5b3afda9da3ed8e926b6a0c1cdfabbe
Jaren Garman <jarengarman@protonmail.com>
2025-04-28 13:22:50 -0400

    feat: Add Subscription model and migration

---
+++ db/migrate/20250428172128_create_subscriptions.rb
@@ -0,0 +1,12 @@
+class CreateSubscriptions < ActiveRecord::Migration[7.1]
+  def change
+    create_table :subscriptions do |t|
+      t.string :title
+      t.float :price
+      t.string :image_url
+      t.string :string
+
+      t.timestamps
+    end
+  end
+end
---
+++ spec/factories/subscriptions.rb
@@ -0,0 +1,8 @@
+FactoryBot.define do
+  factory :subscription do
+    title { "MyString" }
+    price { 1.5 }
+    image_url { "MyString" }
+    string { "MyString" }
+  end
+end
